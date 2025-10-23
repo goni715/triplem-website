43e4393574c749d845b83461adc4ae8905d8f9aa
goni715 <halesgoni715149123@gmail.com>
2025-10-23 17:07:03 +0600

    update product filter

--- src/components/shop/MobileFilter.jsx
+++ src/components/shop/MobileFilter.jsx
@@ -75,8 +75,8 @@ const MobileFilter = ({categories, categoryLoading, priceRange, selectedRating, 
                                                 <Label htmlFor="r2-mobile" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-subtitle">Out of Stock</Label>
                                             </div>
                                             <div className="flex items-center gap-3">
-                                                <RadioGroupItem value="up_coming" id="r3-mobile" />
-                                                <Label htmlFor="r3-mobile" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-subtitle">Up Coming</Label>
+                                                <RadioGroupItem value="limited_stock" id="r3-mobile" />
+                                                <Label htmlFor="r3-mobile" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-subtitle">Limited Stock</Label>
                                             </div>
                                         </RadioGroup>
                                     </AccordionContent>
